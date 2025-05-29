3d0943cf9dc883cdda3560cb031288bbc3166727
Marvin Horitani <marvin.horitani@gmail.com>
2025-05-29 18:55:45 +0200

    chore: install tanstack-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.tsx";
 
@@ -17,10 +18,14 @@ async function enableMocking() {
   });
 }
 
+const queryClient = new QueryClient();
+
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   );
 });
